146bcd9d871ac10d23df95b67e888abdea19ff10
Baka <one.and.only.baka@gmail.com>
2022-12-26 20:46:22 +0100

    Added autostart for raspbian to setup script

--- setup_raspbian.sh
+++ setup_raspbian.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+START_COFFEEPY_BASH=$SCRIPT_DIR"/StartCoffeePy.sh"
+AUTOSTART_PATH="~/.config/autostart/"
+AUTOSTART_FILE=$SCRIPT_DIR"/CoffeePy.desktop"
+
 wget http://deb.debian.org/debian/pool/main/t/tcl-awthemes/tcl-awthemes_10.4.0.orig.tar.xz
 tar -xf tcl-awthemes_10.4.0.orig.tar.xz -C gui/
 rm tcl-awthemes_10.4.0.orig.tar.xz
 
-sudo apt-get -y install swig python3 python3-tk python3-pil python3-pil.imagetk python3-pyscard python3-pip pcscd autoconf debhelper flex libusb-dev libpcsclite-dev libpcsclite1 libccid pcscd pcsc-tools libpcsc-perl libusb-1.0-0-dev libtool libssl-dev cmake checkinstall
+sudo apt-get -y install swig python3 python3-tk python3-pil python3-pil.imagetk python3-pyscard python3-pip pcscd autoconf debhelper flex libusb-dev libpcsclite-dev libpcsclite1 libccid pcscd pcsc-tools libpcsc-perl libusb-1.0-0-dev libtool libssl-dev cmake xterm checkinstall
 
 git clone https://github.com/nfc-tools/libnfc
 sudo mkdir -p /etc/nfc/devices.d
@@ -19,5 +24,18 @@ rm -rf libnfc
 sudo sh -c 'echo blacklist pn533_usb >> /etc/modprobe.d/blacklist-nfc.conf'
 
 pip install pyscard
+
+~/.config/autostart/coffeepy.desktop
+
+echo "#!/bin/bash" > $START_COFFEEPY_BASH
+echo "cd "$SCRIPT_DIR >> $START_COFFEEPY_BASH
+echo "/bin/python3 CoffeePy.py" >> $START_COFFEEPY_BASH
+
+mkdir -p $AUTOSTART_PATH
+echo "[Desktop Entry]" > $AUTOSTART_FILE
+echo "Type=Application" >> $AUTOSTART_FILE
+echo "Name=CoffeePyApp" >> $AUTOSTART_FILE
+echo "Exec=xterm -hold -e 'sh "$START_COFFEEPY_BASH"'" >> $AUTOSTART_FILE
+echo "Terminal=false" >> $AUTOSTART_FILE
 
 echo "Restart is required..."
